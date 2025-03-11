31559dbe47e9dde2b513fd583e2485c8d2e3f4d0
imkunet <im@kunet.dev>
2025-03-11 18:10:00 -0400

    feat: what to add in terms of cache

--- flake.nix
+++ flake.nix
@@ -9,4 +9,23 @@
     {
       homeManagerModules.bkunet = import ./home;
     };
+
+  # you probably want these lines in your flake.nix
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://catppuccin.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nixvim.cachix.org"
+      "https://statix.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixvim.cachix.org-1:8xrm/43sWNaE3sqFYil49+3wO5LqCbS4FHGhMCuPNNA="
+      "statix.cachix.org-1:Z9E/g1YjCjU117QOOt07OjhljCoRZddiAm4VVESvais="
+    ];
+  };
 }
